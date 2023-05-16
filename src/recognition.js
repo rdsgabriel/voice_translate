efffb9ec01e6086d99feadf660c7e8a5ed2e533e
rdsgabriel <gabriel.rodrigues.ismart@gmail.com>
2023-05-15 21:19:37 -0300

    done

--- src/recognition.js
+++ src/recognition.js
@@ -28,9 +28,9 @@ recognition.onresult = (event) => {
         .then(response => response.json())
         .then(data => {
           const translatedText = data.responseData.translatedText;
-          const createParagraph = document.createElement('h1');
-          createParagraph.textContent = translatedText;
-          document.body.appendChild(createParagraph);
+          const textarea = document.querySelector('textarea');
+          const currentValue = textarea.value
+           textarea.value = `${currentValue ? `${currentValue}\n` : ''}${translatedText}`;
 
           // Limpa o último texto e reinicia o reconhecimento de fala
           lastText = '';
@@ -50,16 +50,17 @@ recognition.onerror = (event) => {
 
     startButton.addEventListener('click', () => {
       console.log('Reconhecimento de fala iniciado')
+      startButton.innerHTML = '<svg style="color: red" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-mic" viewBox="0 0 16 16"> <path d="M3.5 6.5A.5.5 0 0 1 4 7v1a4 4 0 0 0 8 0V7a.5.5 0 0 1 1 0v1a5 5 0 0 1-4.5 4.975V15h3a.5.5 0 0 1 0 1h-7a.5.5 0 0 1 0-1h3v-2.025A5 5 0 0 1 3 8V7a.5.5 0 0 1 .5-.5z" fill="red"></path> <path d="M10 8a2 2 0 1 1-4 0V3a2 2 0 1 1 4 0v5zM8 0a3 3 0 0 0-3 3v5a3 3 0 0 0 6 0V3a3 3 0 0 0-3-3z" fill="red"></path> </svg>'
+      startButton.classList.add('bg-white')
+      startButton.classList.add('animate-bounce')
       recognition.start()
   })
 
-  const stopButton = document.getElementById('stop-button')
-  
-     stopButton.addEventListener('click', () => {
-      recognition.onend = () => {
-        console.log('Reconhecimento de fala encerrado');
-      }
-  })
 
+  recognition.onend = (e) => {
+    startButton.classList.remove('bg-white')
+    startButton.classList.remove('animate-bounce')
+    startButton.innerHTML = 'Start'
+  }
 
 
